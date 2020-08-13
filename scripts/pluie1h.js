0d335a1b9ea214a588029f3ca25a431afe86a421
flyingeek <edelord@gmail.com>
2020-08-13 11:40:58 +0200

    bug fix

--- scripts/pluie1h.js
+++ scripts/pluie1h.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var log = require("log");
 var cache = require("/lib/cache");
-log.setLevel("INFO"); //levels are ERROR | WARN | INFO | DEBUG | OFF
+log.setLevel("DEBUG"); //levels are ERROR | WARN | INFO | DEBUG | OFF
 var frames = [];
 var icon = "a3361";
 var location = request.parameters.location || "333180";
@@ -11,7 +11,7 @@ function meteo1Request() {
             "params": {}
     });
 }
-var api = cache.getCache(meteo1Request, "meteo1hb_" + location, 300, 300);
+var api = cache.getCache(meteo1Request, "meteo1h_" + location, 300, 300);
 var api = meteo1Request();
 if (api.status != 200) {
         frames.push({"text": "ERROR", "icon": icon});
@@ -19,24 +19,24 @@ if (api.status != 200) {
         var json = JSON.parse(api.body);
         var data = json["result"]['intervalles'];
         var data5 = [];
-        data5.push(data[0][1]);//0 0-5
-        data5.push(data[1][1]);//1 5-10
-        data5.push(data[2][1]);//2 10-15
-        data5.push(data[3][1]);//3 15-20
-        data5.push(data[4][1]);//4 20-25
-        data5.push(data[5][1]);//5 25-30
-        data5.push(data[6][1]);//6 30-35
-        data5.push(data[7][1]);//7 35-40
-        data5.push(data[7][1]);//8 40-45
-        data5.push(data[8][1]);//9 45-50
-        data5.push(data[8][1]);//10 50-55
-        data5.push(data[9][1]);//11 55-60
-        data5.push(data[9][1]);//12 60-65
+        data5.push(data[0].value);//0 0-5
+        data5.push(data[1].value);//1 5-10
+        data5.push(data[2].value);//2 10-15
+        data5.push(data[3].value);//3 15-20
+        data5.push(data[4].value);//4 20-25
+        data5.push(data[5].value);//5 25-30
+        data5.push(data[6].value);//6 30-35
+        data5.push(data[7].value);//7 35-40
+        data5.push(data[7].value);//8 40-45
+        data5.push(data[8].value);//9 45-50
+        data5.push(data[8].value);//10 50-55
+        data5.push(data[9].value);//11 55-60
+        data5.push(data[9].value);//12 60-65
         var chartData = data5.map(function(v) {
                 var level = parseInt(v, 10);
                 return (level <= 1) ? 0 : level * 2;
         });
-        var delay = data.findIndex(function(v) {
+        var delay = chartData.findIndex(function(v) {
              return v >= 2;
         });
         var chartFrame = {"index": 0, "chartData": chartData};
